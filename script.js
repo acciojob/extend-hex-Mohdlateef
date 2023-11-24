7aa4f6b20a1629c218194c4edc00405a7273037a
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2023-11-24 16:01:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let str=""
+	for(let i=0;i<shortHex.lenght;i++)
+		{
+			if(charAt(i)>='a'&&charAt(i)<='z')
+			{
+				str=str+charAt(i)+charAt(i);
+				
+			}
+			else
+			{}
+		}
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+// const shortHex = prompt("Enter Short Hex.");
+// alert(extendHex(shortHex));
